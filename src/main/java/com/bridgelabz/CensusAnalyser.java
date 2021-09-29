4308df5ad5bf77d449050b3d7893814d2b0c5d36
SUDIPP22 <panja370@gmail.com>
2021-09-30 01:18:29 +0530

    Added : Welcome Message

---
+++ src/main/java/com/bridgelabz/CensusAnalyser.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+/**
+ * Purpose : To implement Indian State Census Analyser
+ *
+ * @author SUDIP PANJA
+ * @version 11.0.11
+ * @since 2021-09-29
+ */
+public class CensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome To The Program");
+    }
+}
