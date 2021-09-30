8a8c3458e20ef232324c6f7b99864b8e89c353f0
SUDIPP22 <panja370@gmail.com>
2021-09-30 14:38:27 +0530

    Added : Test cases and exceptions handling to read State Census file

--- src/main/java/com/bridgelabz/CensusAnalyser.java
+++ src/main/java/com/bridgelabz/CensusAnalyser.java
@@ -1,5 +1,9 @@
 package com.bridgelabz;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Purpose : To implement Indian State Census Analyser
  *
@@ -8,7 +12,22 @@ package com.bridgelabz;
  * @since 2021-09-29
  */
 public class CensusAnalyser {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, CensusAnalyserException {
         System.out.println("Welcome To The Program");
+        String filePathRead = ".src/main/resources/IndianStateCensusData.csv";
+        String fileName = "IndianStateCensusData";
+        String delimiter =",";
+        List<String> stringName = new ArrayList<>();
+        stringName.add("State");
+        stringName.add("Population");
+        stringName.add("AreaInSqKm");
+        stringName.add("DensityPerSqKm");
+
+        ReadOperations readObj = new ReadOperations();
+        int count = readObj.readDataCount(filePathRead, fileName);
+        System.out.println(count);
+
+        readObj.readDelimiter(filePathRead, delimiter);
+        readObj.readHeader(filePathRead, stringName);
     }
 }
